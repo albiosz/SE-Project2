009976bfa826140b40caca01d754542e67de7af4
moreno <moreno.marrara@stud.fh-campuswien.ac.at>
2025-04-27 23:02:44 +0200

    MM20250427
    changed lambda_handler accordingly

--- AWS/Lambdas/cars_GET_one/lambda_function.py
+++ AWS/Lambdas/cars_GET_one/lambda_function.py
@@ -1,9 +1,13 @@
-# /api/v1/cars GET
+# /api/v1/cars/{car_id} GET
 
+from exceptions.cars_exceptions import InvalidCarIdException
 from service.cars_service import CarService
 
 def lambda_handler(event, context):
-    return CarService().get_all_cars()
-
+    car_id = event.get('car_id')
+    if car_id is None or not car_id.isnumeric():
+        raise InvalidCarIdException(car_id)
+    else:
+        return CarService().get_car_by_id(int(car_id))
 
 
